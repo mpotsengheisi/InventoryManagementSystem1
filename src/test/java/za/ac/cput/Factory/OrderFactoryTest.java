6ab9c34c5f42690f4e8db4b94cfbc8931ac618f1
likradebe05 <222804424@mycput.ac.za>
2025-05-18 23:36:36 +0200

    Test Classes

---
+++ src/test/java/za/ac/cput/Factory/OrderFactoryTest.java
@@ -0,0 +1,22 @@
+package za.ac.cput.Factory;
+
+import za.ac.cput.Domain.Order;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.Test;
+
+/*
+Author: Luhlume Iarlaith Keamogetse Radebe
+Date: 18 May 2025
+OrderFactoryTest.java
+ */
+
+public class OrderFactoryTest {
+    @Test
+    public void createOrder() {
+        Order order = OrderFactory.createOrder("O001", "C123", "Laptop", "Out for delivery", "Adderly Street", "Card");
+        System.out.println("Created Order: " + order);
+        assertNotNull(order.getOrderId());
+    }
+
+}
