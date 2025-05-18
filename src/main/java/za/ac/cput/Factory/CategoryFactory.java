b4245536720f0ef39bba6e4131954f96d538f7ab
Ahluma-Nkqayi <222512571@mycput.ac.za>
2025-05-18 14:21:46 +0200

    Category and Supplier Factory classes

---
+++ src/main/java/za/ac/cput/Factory/CategoryFactory.java
@@ -0,0 +1,23 @@
+package za.ac.cput.Factory;
+/*Category.java
+Category Factory class
+Author: Ahluma Nkqayi (222512571)
+Date: 18 May 2025
+ */
+import za.ac.cput.Domain.Category;
+
+public class CategoryFactory {
+    public static Category createCategory(String categoryId, String description) {
+        if (!Helper.isValidCategoryData(categoryId, description)) {
+            return null;
+        }
+
+        return new Category.Builder()
+                .setCategoryId(categoryId)
+                .setDescription(description)
+                .build();
+    }
+}
+
+
+
